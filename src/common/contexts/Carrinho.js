9eb979f3e3bac3ccb1304d5641a1d0dcf4e47f29
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-04-13 16:48:49 -0300

    Criando a função de calcular o valor total do carrinho

--- src/common/contexts/Carrinho.js
+++ src/common/contexts/Carrinho.js
@@ -7,16 +7,17 @@ export const CarrinhoProvider = ({ children }) => {
 
   const [ carrinho, setCarrinho ] = useState([]);
   const [quantidadeProdutos, setQuantidadeProdutos] = useState(0);
+  const [ valorTotalCarrinho, setValorTotalCarrinho ] = useState(0);
 
   return (
-    <CarrinhoContext.Provider value={{ carrinho, setCarrinho, quantidadeProdutos, setQuantidadeProdutos }}>
+    <CarrinhoContext.Provider value={{ carrinho, setCarrinho, quantidadeProdutos, setQuantidadeProdutos, valorTotalCarrinho, setValorTotalCarrinho }}>
       {children}
     </CarrinhoContext.Provider>
   );
 }
 
 export const useCarrinhoContext = () => {
-  const { carrinho, setCarrinho, quantidadeProdutos, setQuantidadeProdutos } = useContext(CarrinhoContext);
+  const { carrinho, setCarrinho, quantidadeProdutos, setQuantidadeProdutos, valorTotalCarrinho, setValorTotalCarrinho } = useContext(CarrinhoContext);
 
   function mudarQuantidade(id, quantidade) {
     return carrinho.map(itemCarrinho => {
@@ -47,9 +48,16 @@ export const useCarrinhoContext = () => {
   }
 
   useEffect(() => {
-    const novaQuantidade = carrinho.reduce((contador, produto) => contador + produto.quantidade, 0);
+    const { novaQuantidade, novoTotal } = carrinho.reduce((contador, produto) => ({
+      novaQuantidade: contador.novaQuantidade + produto.quantidade,
+      novoTotal: contador.novoTotal + ( produto.valor * produto.quantidade )
+    }), {
+      novaQuantidade: 0,
+      novoTotal: 0,
+    });
     setQuantidadeProdutos(novaQuantidade);
-  }, [carrinho, setQuantidadeProdutos]);
+    setValorTotalCarrinho(novoTotal)
+  }, [carrinho, setQuantidadeProdutos, setValorTotalCarrinho]);
 
   return {
     carrinho, 
@@ -58,5 +66,6 @@ export const useCarrinhoContext = () => {
     removerProduto,
     quantidadeProdutos,
     setQuantidadeProdutos,
+    valorTotalCarrinho,
   }
 }
