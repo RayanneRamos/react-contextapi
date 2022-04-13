6bdec81b2e6b8db71c297dd06c5ad5f502449983
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-04-12 22:18:48 -0300

    Criando a função de removerProduto e de mudarQuantidade

--- src/common/contexts/Carrinho.js
+++ src/common/contexts/Carrinho.js
@@ -17,23 +17,38 @@ export const CarrinhoProvider = ({ children }) => {
 export const useCarrinhoContext = () => {
   const { carrinho, setCarrinho } = useContext(CarrinhoContext);
 
+  function mudarQuantidade(id, quantidade) {
+    return carrinho.map(itemCarrinho => {
+      if(itemCarrinho.id === id) {
+        itemCarrinho.quantidade += quantidade;
+      }
+      return itemCarrinho;
+    })
+  }
+
   function adicionarProduto(novoProduto) {
     const temProduto = carrinho.some(itemCarrinho => itemCarrinho.id === novoProduto.id);
     if(!temProduto) {
       novoProduto.quantidade = 1;
       return setCarrinho(carrinhoAnterior => [...carrinhoAnterior, novoProduto]);
     }
-    setCarrinho(carrinhoAnterior => carrinhoAnterior.map(itemCarrinho => {
-      if(itemCarrinho.id === novoProduto.id) {
-        itemCarrinho.quantidade += 1;
-      }
-      return itemCarrinho;
-    })) 
+    setCarrinho(mudarQuantidade(novoProduto.id, 1)); 
+  }
+
+  function removerProduto(id) {
+    const produto = carrinho.find(itemCarrinho => itemCarrinho.id === id);
+    const ultimoProduto = produto.quantidade === 1;
+    if(ultimoProduto) {
+      return setCarrinho(carrinhoAnterior => carrinhoAnterior.filter(itemCarrinho => itemCarrinho.id !== id));
+    }
+
+    setCarrinho(mudarQuantidade(id, -1));
   }
 
   return {
     carrinho, 
     setCarrinho, 
     adicionarProduto,
+    removerProduto,
   }
 }
