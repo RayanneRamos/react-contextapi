86314c73c966a0f1c8585bdfb711ca84ffce5ae6
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-04-13 19:18:34 -0300

    Desabilitando o botão comprar quando o carrinho estiver vazio

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -62,7 +62,7 @@ function Carrinho() {
         }}
         color="primary"
         variant="contained"
-        disabled={total < 0}
+        disabled={total < 0 || carrinho.length === 0}
       >
          Comprar
        </Button>
